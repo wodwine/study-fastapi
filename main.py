e780c8fd521398085c4c1efde3a6f2de0eeb1db2
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-03-30 09:19:42 +0700

    Path Parameters

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def index():
     return {"Hello": "World"}
 
 
 @app.get("/about")
 def about():
-    return "This API is used for learn about FastAPI"
+    return {"data": "This API is used for learn about FastAPI"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "This is a secret"}
+
+
+@app.get("/blog/{blog_id}")
+def blog(blog_id: int):
+    return {"Blog": blog_id}
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+    return {"Blog": blog_id, "comment": "Hello World"}
