8ddd93c8ca5b9ff6697b9614803d7170936f7420
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-03-30 17:07:19 +0700

    JWT Access Token

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from .. import schemas, database, models
+from .. import schemas, database, models,token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
 
@@ -15,4 +15,5 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Incorrect password")
-    return user
+    access_token = token.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class BlogBase(BaseModel):
@@ -45,3 +45,12 @@ class ShowBlog(Blog):
 class Login(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
