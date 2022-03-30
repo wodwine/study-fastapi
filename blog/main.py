1ec91eb3543c6ceb9b795f7ee18c08cd92022350
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-03-30 12:53:09 +0700

    Exception and status code

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, HTTPException, status
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -22,7 +22,7 @@ def index():
     return "Blog API"
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -37,9 +37,14 @@ def get_all_blog(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{blog_id}")
-def get_blog_by_id(blog_id: int, db: Session = Depends(get_db)):
+@app.get("/blog/{blog_id}", status_code=200)
+def get_blog_by_id(blog_id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id: {blog_id} is not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with the id: {blog_id} is not found"}
     return blog
 
 
