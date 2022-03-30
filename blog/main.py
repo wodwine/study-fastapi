7573fdf7713c7469be2f170d67bdd0450be85580
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-03-30 12:42:01 +0700

    Query all and one row

--- blog/main.py
+++ blog/main.py
@@ -23,12 +23,24 @@ def index():
 
 
 @app.post("/blog")
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def get_all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{blog_id}")
+def get_blog_by_id(blog_id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    return blog
 
 
 if __name__ == "__main__":
